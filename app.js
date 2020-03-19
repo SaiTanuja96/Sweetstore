232e84d7971a76cfd43a781662819e4c4117ad3f
Tanuja Batchali <34961894+SaiTanuja96@users.noreply.github.com>
2020-03-19 19:29:53 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+
+
+//document.getElementById('cart-info').addEventListener("click", function() {
+//	document.querySelector('.cart').style.display = "flex";
+//});//
+(function(){
+	const cartInfo = document.getElementById("cart-info");
+	const cart = document.getElementById("cart");
+	
+	cartInfo.addEventListener("click", function() {
+	cart.classList.toggle("show-cart");
+
+})
+})();
+
+// add items to the cart
+
+(function(){
+
+	const cartBtn = document.querySelectorAll('.store-item-icon');
+cartBtn.forEach(function(btn){
+btn.addEventListener('click',function(event){
+
+console.log(event.target);	
+})
+
+})
+
+
+
+
+
+})();
+
+
+
+
